9e6fcd5e52509ea0c29ee9a382d59f22d34a21f2
Nikita <nikitakatrich.kzn@gmail.com>
2026-02-10 17:42:57 +0300

    build entire structure of HTML, added images and some icons

--- assets/scripts/queryToServer.js
+++ assets/scripts/queryToServer.js
@@ -1,7 +1,10 @@
 const APIKey = '3d8e7f9f1caac1280ad2e13b68cc7d70';
-const baseURL = 'https://api.openweathermap.org/data/2.5/forecast';
 
-let properURL = buildWeatherUrl('казань');
+const baseURLWeather = 'https://api.openweathermap.org/data/2.5/weather';
+const baseURLForecast = 'https://api.openweathermap.org/data/2.5/forecast';
+const baseURLGeo = 'https://api.openweathermap.org/geo/1.0/direct';
+
+let properURL = buildWeatherURL('казань');
 
 fetch(properURL).then(res => {
     if (!res.ok) {
@@ -9,11 +12,11 @@ fetch(properURL).then(res => {
     }
     return res.json();
 }).then(data => {
-    console.log(data.list);
+    console.log(data);
 }).catch(err => {
     console.error(err);
 })
 
-function buildWeatherUrl(city) {
-    return `${baseURL}?q=${city}&units=metric&lang=ru&appid=${APIKey}`;
+function buildWeatherURL(city) {
+    return `${baseURLWeather}?q=${city}&units=metric&lang=ru&appid=${APIKey}`;
 }
